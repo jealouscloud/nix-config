c67cf45148731ba3a762c8f1458a064f2b75a197
jealouscloud <nablaseau@hotmail.com>
2024-03-17 00:12:15 -0400

    Lets try this

--- common/configuration.nix
+++ common/configuration.nix
@@ -7,20 +7,6 @@
   pkgs,
   ...
 }: {
-  # You can import other NixOS modules here
-  imports = [
-    # If you want to use modules from other flakes (such as nixos-hardware):
-    # inputs.hardware.nixosModules.common-cpu-amd
-    # inputs.hardware.nixosModules.common-ssd
-
-    # You can also split up your configuration and import pieces of it here:
-    # ./users
-
-    # Import your generated (nixos-generate-config) hardware configuration
-    ./audio.nix
-    ./locale.nix
-  ];
-
   # NOTE: the following are two ways to say the same thing
   # nixpkgs.config.allowUnfree = true;
 
--- hosts/nixos-vm/default.nix
+++ hosts/nixos-vm/default.nix
@@ -1,7 +1,15 @@
-{ pkgs, inputs, ... }: {
+{
+  inputs,
+  lib,
+  config,
+  pkgs,
+  ...
+}: {
   imports = [
     ./hardware-configuration.nix
     ../common/configuration.nix
+    ../common/audio.nix
+    ../common/locale.nix
     ./virtualized.nix
   ];
 }

3da4c909a070a6956b67d971f357580ca7267e19
jealouscloud <nablaseau@hotmail.com>
2024-03-17 00:23:09 -0400

    Fix relative import

--- hosts/nixos-vm/default.nix
+++ hosts/nixos-vm/default.nix
@@ -7,7 +7,7 @@
 }: {
   imports = [
     ./hardware-configuration.nix
-    ../common/configuration.nix
+    ../../common/configuration.nix
     # ../common/audio.nix
     # ../common/locale.nix
     # ./virtualized.nix

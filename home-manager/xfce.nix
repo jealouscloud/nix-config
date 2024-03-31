24c64a63c1ace032bd067a08bfe1d9802eac4fae
jealouscloud <nablaseau@hotmail.com>
2024-03-31 14:19:09 -0400

    add dashboard

--- home-manager/xfce.nix
+++ home-manager/xfce.nix
@@ -1,3 +1,9 @@
+{ pkgs
+, ...
+}:
 {
   xdg.configFile."xfce4/xfconf/xfce-perchannel-xml/xfce4-keyboard-shortcuts.xml".source = ./files/xfce/xfce4-keyboard-shortcuts.xml;
+  home.packages = (with pkgs; [
+    xfce.xfdashboard
+  ]);
 }

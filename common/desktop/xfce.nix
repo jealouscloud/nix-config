e33f29599b4137b2bbafe20c84acf0734b532171
jealouscloud <nablaseau@hotmail.com>
2024-03-31 13:57:58 -0400

    Add keybinds

--- common/desktop/xfce.nix
+++ common/desktop/xfce.nix
@@ -11,4 +11,6 @@
     layout = "us";
     xkbVariant = "";
   };
+  xdg.configFile."xfce4/xfconf/xfce-perchannel-xml/xfce4-keyboard-shortcuts.xml".source = ./files/xfce4-keyboard-shortcuts.xml;
+
 }

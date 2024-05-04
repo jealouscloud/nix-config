ba5084a85c855c486f001214d927a21bfb9b6a1c
Noah <developer@macbook>
2024-05-04 13:23:03 -0400

    Fuse filesystem that returns symlinks to executables

--- common/configuration.nix
+++ common/configuration.nix
@@ -79,6 +79,11 @@
   # Enable CUPS to print documents.
   services.printing.enable = true;
 
+  # A fuse filesystem that dynamically populates contents of 
+  # /bin and /usr/bin/ so that it contains all executables from the PATH of the requesting process. This allows executing FHS based programs on a non-FHS system. 
+  # For example, this is useful to execute shebangs on NixOS that assume hard coded locations like /bin or /usr/bin etc.
+  services.envfs.enable = true;
+
   # get mtr
   programs.mtr.enable = true;
 

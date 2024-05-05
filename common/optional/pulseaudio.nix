e55338ea3ac0036a8bd8b87efc38dd75f9efa24e
jealouscloud <nablaseau@hotmail.com>
2024-05-05 12:57:17 -0400

    Add pulseaudio module

---
+++ common/optional/pulseaudio.nix
@@ -0,0 +1,7 @@
+{
+  # https://nixos.wiki/wiki/PulseAudio
+  sound.enable = true;
+  hardware.pulseaudio.enable = true;
+  hardware.pulseaudio.support32Bit = true;
+  hardware.pulseaudio.extraConfig = "load-module module-combine-sink";
+};

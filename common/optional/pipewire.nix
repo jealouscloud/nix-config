12eb38ef7400e9cefe6561093afad6b12bf5d42a
Noah <developer@macbook>
2024-05-05 13:00:05 -0400

    syntax fixes

--- common/optional/pipewire.nix
+++ common/optional/pipewire.nix
@@ -7,9 +7,9 @@
     alsa.enable = true;
     alsa.support32Bit = true;
     pulse.enable = true;
+    jack.enable = true;
   };
-  jack.enable = true;
   # use the example session manager (no others are packaged yet so this is enabled by default,
   # no need to redefine it in your config for now)
   #media-session.enable = true;
-};
+}

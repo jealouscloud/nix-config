57fd56962d76423302a39430b859c4bcb525ce94
Noah <developer@macbook>
2024-06-26 16:53:17 -0400

    Set gpg pinentry

--- home-manager/rofi.nix
+++ home-manager/rofi.nix
@@ -2,6 +2,9 @@
 pkgs
 , ...
 }: {
+    # Some references:
+    # https://github.com/nix-community/home-manager/blob/master/modules/programs/rofi.nix
+    # https://github.com/nmasur/dotfiles/blob/master/modules/nixos/graphical/rofi.nix
     programs.rofi = {
         enable = true;
         theme = "~/.local/share/rofi/dracula/config1.rasi";
@@ -10,4 +13,5 @@ pkgs
         recursive = true;
         source = ./files/rofi;
     };
+    services.gpg-agent.pinentryPackage = pkgs.pinentry-rofi;
 }

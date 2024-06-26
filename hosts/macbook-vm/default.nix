ba8cc86836a68d72e2ce97e6ececb510058192a5
Noah <developer@macbook>
2024-06-26 16:56:37 -0400

    macbook: prefer pulse

--- hosts/macbook-vm/default.nix
+++ hosts/macbook-vm/default.nix
@@ -9,7 +9,7 @@
     ./hardware-configuration.nix
     ../../common/configuration.nix
     ../../common/optional/desktop/xfce.nix
-    ../../common/optional/pipewire.nix
+    ../../common/optional/pulseaudio.nix
     ../../common/locale.nix
     ./virtualized.nix
   ];

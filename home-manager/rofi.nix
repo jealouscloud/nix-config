b3a1fb9068b296b45754cb58d1688bdd4f19581f
Noah <developer@macbook>
2024-06-26 16:17:44 -0400

    Add dracula theme

--- home-manager/rofi.nix
+++ home-manager/rofi.nix
@@ -1,7 +1,12 @@
-{
- pkgs, ...
+{ 
+pkgs
+, ...
 }: {
     programs.rofi = {
         enable = true;
+    };
+    home.file.".local/share/rofi" = {
+        recursive = true;
+        source = ./files/rofi;
     };
 }

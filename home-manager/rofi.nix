8cebdd31f70da9254eb490bd78cf0b0855ad715a
jealouscloud <nablaseau@hotmail.com>
2024-06-26 16:59:25 -0400

    Add refs

--- home-manager/rofi.nix
+++ home-manager/rofi.nix
@@ -5,6 +5,10 @@ pkgs
     # Some references:
     # https://github.com/nix-community/home-manager/blob/master/modules/programs/rofi.nix
     # https://github.com/nmasur/dotfiles/blob/master/modules/nixos/graphical/rofi.nix
+    # https://github.com/JRMurr/NixOsConfig/blob/main/common/homemanager/rofi.nix
+    # https://github.com/nmasur/dotfiles/blob/master/modules/nixos/graphical/rofi.nix
+
+
     programs.rofi = {
         enable = true;
         theme = "~/.local/share/rofi/dracula/config1.rasi";

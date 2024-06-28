e959db9d86180598808d8623a1483220ef489c9a
Noah <developer@macbook>
2024-06-28 17:04:23 -0400

    Add xfce packages from othe internet

--- common/optional/desktop/xfce.nix
+++ common/optional/desktop/xfce.nix
@@ -16,5 +16,23 @@
   environment.systemPackages = with pkgs; [
     xfce.xfce4-volumed-pulse
     xfce.xfce4-pulseaudio-plugin
+    xfce.catfish
+    xfce.gigolo
+    xfce.orage
+    xfce.xfburn
+    xfce.xfce4-appfinder
+    xfce.xfce4-clipman-plugin
+    xfce.xfce4-cpugraph-plugin
+    xfce.xfce4-dict
+    xfce.xfce4-fsguard-plugin
+    xfce.xfce4-genmon-plugin
+    xfce.xfce4-netload-plugin
+    xfce.xfce4-panel
+    xfce.xfce4-pulseaudio-plugin
+    xfce.xfce4-systemload-plugin
+    xfce.xfce4-weather-plugin
+    xfce.xfce4-whiskermenu-plugin
+    xfce.xfce4-xkb-plugin
+    xfce.xfdashboard
   ];
 }

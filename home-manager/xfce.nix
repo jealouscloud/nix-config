bc1e1752aeaba564ef26dfb5fb3572a1ca0a86a6
Noah <developer@macbook>
2024-06-28 17:35:18 -0400

    ^

--- home-manager/xfce.nix
+++ home-manager/xfce.nix
@@ -3,7 +3,17 @@
 }:
 {
   xdg.configFile."xfce4/xfconf/xfce-perchannel-xml/xfce4-keyboard-shortcuts.xml".source = ./files/xfce/xfce4-keyboard-shortcuts.xml;
-  home.packages = (with pkgs; [
-    xfce.xfdashboard
-  ]);
+  # home.packages = (with pkgs; [
+  #   # xfce.xfdashboard
+  # ]);
+  gtk = {
+    enable = true;
+    # iconTheme = {
+
+    # };
+    # theme = {
+    #   name = "something";
+    #   package = pkgs.zuki-themes;
+    # }
+  };
 }

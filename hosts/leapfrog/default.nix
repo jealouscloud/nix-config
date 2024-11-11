6b247afecf1a6aeb1ecdce82aa4b279ceea692ad
jealouscloud <nablaseau@hotmail.com>
2024-11-11 09:51:37 -0500

    Add file

---
+++ hosts/leapfrog/default.nix
@@ -0,0 +1,16 @@
+{
+  inputs,
+  lib,
+  config,
+  pkgs,
+  ...
+}: {
+  imports = [
+    ./hardware-configuration.nix
+    ../../common/configuration.nix
+    ../../common/optional/desktop/xfce.nix
+    ../../common/optional/pipewire.nix
+    ../../common/locale.nix
+    ./virtualized.nix
+  ];
+}

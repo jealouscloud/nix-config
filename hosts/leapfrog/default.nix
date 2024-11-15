b443833105622af1f59fb92d37b1222383e16268
jealouscloud <github@noaha.org>
2024-11-15 12:16:34 -0500

    Add flatpak support

--- hosts/leapfrog/default.nix
+++ hosts/leapfrog/default.nix
@@ -15,6 +15,7 @@
     ../../common/boot-uefi.nix
     ../../common/optional/desktop/xfce.nix
     ../../common/optional/pipewire.nix
+    ../../common/optional/flatpak.nix
     ../../common/locale.nix
   ];
 }

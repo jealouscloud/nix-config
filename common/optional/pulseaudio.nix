94e57af24c506b264f2ba1e972e2065e0a7adc37
jealouscloud <github@noaha.org>
2024-12-03 21:40:13 -0500

    sound was removed

--- common/optional/pulseaudio.nix
+++ common/optional/pulseaudio.nix
@@ -1,6 +1,5 @@
 {
   # https://nixos.wiki/wiki/PulseAudio
-  sound.enable = true;
   hardware.pulseaudio.enable = true;
   hardware.pulseaudio.support32Bit = true;
   hardware.pulseaudio.extraConfig = "load-module module-combine-sink";

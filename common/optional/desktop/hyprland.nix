90595c2660680e7a475723b41b4125ed96867573
jealouscloud <github@noaha.org>
2024-12-04 12:11:10 -0500

    Starting w/ hyprland

--- common/optional/desktop/hyprland.nix
+++ common/optional/desktop/hyprland.nix
@@ -1,18 +1,23 @@
 {pkgs, inputs, ... }: {
   programs.hyprlock.enable = true;
-  programs.hyprland.enable = true;
   # services.hypridle.enable = true;
-  programs.uwsm = {
-      enable = true;
-      waylandCompositors = {
-      prettyName = "Hyprland";
-      comment = "Hyprland compositor managed by UWSM";
-      binPath = "/run/current-system/sw/bin/Hyprland";
-    };
-  };
-  wayland.windowManager.hyprland = {
+  # programs.uwsm = {
+  #     enable = true;
+  #     waylandCompositors = {
+  #     prettyName = "Hyprland";
+  #     comment = "Hyprland compositor managed by UWSM";
+  #     # binPath = "/run/current-system/sw/bin/Hyprland";
+  #   };
+  # };
+  environment.systemPackages = with pkgs; [
+    kitty
+    wofi
+  ];
+  programs.hyprland = {
     enable = true;
     # set the flake package
     package = inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.hyprland;
+    portalPackage = inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.xdg-desktop-portal-hyprland;
+    withUWSM = true;
   };
 }

62c5dc7e0d4f71979c8c0d987c66c8544662efcf
jealouscloud <github@noaha.org>
2025-06-02 14:55:20 -0400

    disable kde shortcut enforcement

--- home-manager/kde.nix
+++ home-manager/kde.nix
@@ -10,7 +10,17 @@
     source = ./files/kde/konsole;
     recursive = true;
   };
-  xdg.configFile."kglobalshortcutsrc".source = ./files/kde/kglobalshortcutsrc;
+  # xdg.configFile."kglobalshortcutsrc".source = ./files/kde/kglobalshortcutsrc;
+  services.sxhkd = {
+    enable = true;
+    keybindings = {
+      "super + w" = "firefox";
+      "super + Return" = "konsole";
+      "Print" = "flameshot gui";
+      "super + p" = "rofi -modi window,run,drun -sidebar-mode -show drun";
+      # "$mod, ;" = "rofi-rbw -t password -a type";
+    };
+  };
   # home.packages = (with pkgs; [
   #   # xfce.xfdashboard
   # ]);

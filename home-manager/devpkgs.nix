bb4bb738e7feeab54d878c61d91fbf5d138e36ca
jealouscloud <github@noaha.org>
2025-10-10 20:16:28 -0400

    Include placeholder for local pkgs
    turns out i didnt need to do this, just
    nix flake update

--- home-manager/devpkgs.nix
+++ home-manager/devpkgs.nix
@@ -1,4 +1,9 @@
-{ pkgs, pkgs-unstable, inputs, system, ... }: {
+{ pkgs, pkgs-unstable, inputs, system, ... }:
+
+let 
+    # Demo: build your own pkg and use it in home.packages
+    # localAichat = pkgs.callPackage ../pkgs/aichat.nix {};
+in {
   home.packages = (with pkgs; [
     # Language server things
     nixfmt-classic # nix code formatter
@@ -6,10 +11,10 @@
     nil # nix language server
     nixd
     shellcheck
-  
+
     devbox
     direnv
-  
+
     lazygit # fun cli for git, remove if you don't use
 
     rye # python dev tool
@@ -17,15 +22,12 @@
 
     bc # cli calculator
     python3
-
-  ]) ++ (with pkgs-unstable;
-  
-    [
+  ]) ++ (with pkgs-unstable; [
     # ai tools
     aichat
     fabric-ai
     libedgetpu
     edgetpu-compiler
     argc # for llm-functions
-    ]);
+  ]);
 }

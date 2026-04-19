a7f422a8b80e2cf0b0ea30ddb83ab3a2898e6500
jealouscloud <github@noaha.org>
2026-04-19 12:07:33 -0400

    fix earlyoom extraArgs quoting

--- common/optional/memory-safety.nix
+++ common/optional/memory-safety.nix
@@ -7,8 +7,8 @@
       freeMemThreshold = 2;
       extraArgs = [
           "-n"
-          "-g" "--avoid '^(X|plasma.*|konsole|kwin|systemd|NetworkManager|wayland|sddm|konsole|ssh)$'"
-          "--prefer '^(electron|libreoffice|gimp|code|spotify|Telegram|Discord|slack)$'"
+          "-g" "--avoid" "^(X|plasma.*|konsole|kwin|systemd|NetworkManager|wayland|sddm|konsole|ssh)$"
+          "--prefer" "^(electron|libreoffice|gimp|code|spotify|Telegram|Discord|slack|fzf)$"
       ];
   };
 }
